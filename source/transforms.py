9a5f0bc2af30038fa5e329ba013f07cd0ee13b21
Elm Forest <timbrightland@gmail.com>
2025-02-13 23:41:27 +0800

    update model

--- source/transforms.py
+++ source/transforms.py
@@ -116,6 +116,29 @@ def train_augm(sample, size=512):
     return A.Compose(augms)(image=sample["image"], mask=sample["mask"])
 
 
+def train_augm_d4(sample, size=512):
+    augms = [
+        # 旋转、平移、缩放
+        A.ShiftScaleRotate(
+            scale_limit=0.2, rotate_limit=45, border_mode=0, p=0.5
+        ),
+
+        # 随机选择 Resize 或 RandomCrop
+        A.OneOf([
+            A.Resize(height=size, width=size, p=1.0),
+            A.RandomSizedCrop(min_max_height=(400, 700), size=(size, size), p=1.0),
+        ], p=1.0),
+
+        # D4 dihedral group transformations
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.RandomRotate90(p=0.5),
+        A.Transpose(p=0.5),
+    ]
+
+    return A.Compose(augms)(image=sample["image"], mask=sample["mask"])
+
+
 def train_augm_binary(sample, size=512):
     augms = [
         # # 旋转、平移、缩放
